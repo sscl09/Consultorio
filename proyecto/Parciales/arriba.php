621b2e02f6826426a20f01236e732c38866cd58a
Sofía Colín <sofiacolinsscl@gmail.com>
2020-01-03 20:37:19 -0600

    Nueva versión del proyecto

--- proyecto/Parciales/arriba.php
+++ proyecto/Parciales/arriba.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" media="screen" />
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="css/estiloFooter.css">
     <link rel="stylesheet" href="css/estiloNavbar.css">
