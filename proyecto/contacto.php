bbff8d907d34156a590708569ede9733323de96b
Sofía Colín <sofiacolinsscl@gmail.com>
2020-01-04 19:39:44 -0600

    Nueva versión del proyecto

--- proyecto/contacto.php
+++ proyecto/contacto.php
@@ -1,7 +1,6 @@
 <?php
   session_start();
   $titulo = "Consultorio Pediátrico";
-  echo $_POST['id-paciente'];
   require_once('Parciales/arriba.php');
   require_once('Parciales/nav.php');
   //require_once('Recursos/conexion.php');
